8fe3d2c4cec8274bf5f8c0350111edfcc2ad19d3
Jan Krejcarek <jan.krejcarek@gmail.com>
2016-11-25 20:49:02 +0100

    Change id generator to table generator
    Table generator is probably better and easier to understand

--- timesheets2/src/main/java/timesheets2/model/Project.java
+++ timesheets2/src/main/java/timesheets2/model/Project.java
@@ -7,14 +7,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
 
 @Entity(name="projects")
 public class Project implements Serializable {
 	
-	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="projects_id_seq")
-	@SequenceGenerator(name="projects_id_seq", sequenceName="projects_id_seq", allocationSize=1)
-	private int id = -1;
+	@TableGenerator(name="Project_Gen",
+			table="identities",
+		    pkColumnName="gen_name",
+		    valueColumnName="gen_val",
+		    pkColumnValue="Project_Gen",
+		    initialValue=1,
+		    allocationSize=3)
+	@Id @GeneratedValue(generator="Project_Gen")
+	private int id;
 	private String name;
 	public int getId() {
 		return id;
